21e8fb1058c9d3e48f85ec53eba33bd3158ce850
Martins Kozlovskis <mkozlovskis@gmail.com>
2017-02-09 18:28:36 +0200

    addded navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { AlertModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]),
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
